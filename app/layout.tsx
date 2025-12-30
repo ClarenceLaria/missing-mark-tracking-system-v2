d7f6360045fea7cd5189b0758d7fe6783aa54a7f
ClarenceLaria <clarencelaria@gmail.com>
2025-12-30 17:14:29 +0300

    Initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,57 @@
+import type { Metadata } from 'next'
+import { Geist, Geist_Mono } from "next/font/google";
+import './globals.css'
+import AuthContext from './context/AuthContext';
+import ToasterContext from './context/ToasterContext';
+import { ThemeProvider } from './components/ThemeProvider';
+import { Toaster } from './components/ui/toaster';
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: 'MMTS MMUST',
+  description: 'Missing Marks Tracking System',
+  icons: {
+    icon: [
+      {
+        url: '/images/icon.png',
+        href: '/images/icon.png',
+      }
+    ],
+  },
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+
+
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <AuthContext>
+          <ToasterContext/>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+            <Toaster/>
+          </ThemeProvider>
+        </AuthContext>
+      </body>
+    </html>
+  )
+}
